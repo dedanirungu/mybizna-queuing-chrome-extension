75547039d8d7c29a8b9f1d3a824a23c8f2777e3c
Dedan Irungu <dedanirungu@gmail.com>
2023-05-23 12:06:40 +0300

    loading testing iframe

--- popup.js
+++ popup.js
@@ -4,14 +4,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
   openButton.addEventListener('click', function() {
     var url = urlInput.value;
-
-    if (url) {
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        var tab = tabs[0];
-        chrome.tabs.executeScript(tab.id, {
-          code: 'document.body.innerHTML = \'<iframe src="' + url + '" style="width:100%;height:100%;border:none;"></iframe>\';'
-        });
-      });
-    }
   });
 });
